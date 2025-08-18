8d717ec1c21af5ef83bacad230691f007e3981a4
Fiwe64 <wellington.honneyfilho@gmail.com>
2025-08-17 21:08:38 -0400

    Primeiro Commit: Adicionando a estrutura inicial do projeto Spring boot

---
+++ src/main/java/com/FisioNorteSaude/SistemaDeAgendamento/SistemaDeAgendamentoApplication.java
@@ -0,0 +1,13 @@
+package com.FisioNorteSaude.SistemaDeAgendamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeAgendamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeAgendamentoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/FisioNorteSaude/SistemaDeAgendamento/entities/User.java
@@ -0,0 +1,7 @@
+package com.FisioNorteSaude.SistemaDeAgendamento.entities;
+
+public abstract class User {
+    private Long id;
+    private String name;
+    private LocalDate birth
+}
